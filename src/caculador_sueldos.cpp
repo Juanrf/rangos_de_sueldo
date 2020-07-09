c79ece691db90e0a8b6fc9a8f828347fe75cd51d
Juanrf <juanr_fuentes@hotmail.com>
2020-07-08 19:07:13 -0600

    incicando el programa con un saludo

--- src/caculador_sueldos.cpp
+++ src/caculador_sueldos.cpp
@@ -2,7 +2,7 @@
 #include <string>
 using namespace  std;
 int main(){
-    string nombre;
+    string nombre;//iniciando el programa saludando al uaurio
     cout<<"ingrese su nombre: "<<endl;
     getline(cin, nombre);
     cout<<"Hola "<<nombre<<endl;
